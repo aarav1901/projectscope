33e92532ebfe25f369fd9831f4e6109c6e82da37
arav <aravindhanitha847@gmail.com>
2024-06-27 21:25:25 -0700

    first update

---
+++ Controllers/contact.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace firstproject.Controllers
+{
+    public class contact : Controller
+    {
+        public IActionResult page1()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Classpro.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace firstproject.Models
+{
+    public class Classpro
+    {
+        [Required(ErrorMessage ="name is required")]
+        public string full_name{ get; set; }
+        [Required(ErrorMessage="Error is required")]
+        public string email_id {  get; set; }
+        public string subject { get; set; }
+        public string message{ get; set;} 
+            
+    }
+}
